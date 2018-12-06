048a188fc58ba3f93855749cd14f4eed6f3bc286
eeywr@nottingham.ac.uk <eeywr@nottingham.ac.uk>
2018-12-06 15:43:32 +0000

    Vector_created

--- Base Classes/Vector.cpp
+++ Base Classes/Vector.cpp
@@ -1,9 +1,13 @@
+#include <iostream>
 #include "Vector.h"
 
 
 
 Vector::Vector()
 {
+	value1 = 0.0;
+	value2 = 0.0;
+	value3 = 0.0;
 }
 
 
@@ -11,17 +15,22 @@ Vector::~Vector()
 {
 }
 
-float Vector::getVectorX()
-{
-	return x;
+Vector::Vector(float x, float y, float z) {
+	value1 = x;
+	value2 = y;
+	value3 = z;
+	
 }
 
-float Vector::getVectorY()
-{
-	return y;
+float Vector::getvalue_x() const {
+	return value1;
 }
 
-float Vector::getVectorZ()
-{
-	return z;
+float Vector::getvalue_y() const {
+	return value2;
 }
+
+float Vector::getvalue_z() const {
+	return value3;
+}
+
--- Base Classes/Vector.h
+++ Base Classes/Vector.h
@@ -1,13 +1,33 @@
 #pragma once
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+#ifndef Vector_H
+#define Vector_H
+
+
+
 class Vector
 {
 public:
-	float x, y, z;
-
 	Vector();
+	Vector(float, float, float);
 	~Vector();
-	float getVectorX();
-	float getVectorY();
-	float getVectorZ();
+
+	float getvalue_x() const;
+	float getvalue_y() const;
+	float getvalue_z() const;
+	float getvalue_a() const;
+	float getvalue_b() const;
+	float getvalue_c() const;
+
+private:
+	float value1;
+	float value2;
+	float value3;
 };
 
+#endif // !Vector_H
