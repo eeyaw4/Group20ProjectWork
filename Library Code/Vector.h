8c23ade7c4f8343a74af772627cde6d16ad0bdc8
eeywr@nottingham.ac.uk <eeywr@nottingham.ac.uk>
2018-12-07 14:22:29 +0000

    update for push

--- Library Code/Vector.h
+++ Library Code/Vector.h
@@ -1,15 +1,38 @@
 #pragma once
-
+#include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
+
+#ifndef Vector_H
+#define Vector_H
+
+
+
 class Vector
 {
 public:
-	vector<float> xyz;
 	Vector();
+	Vector(float x, float y, float z);
 	~Vector();
-	vector<float> getVector();
+
+
+	void add( Vector v );
+	void print();
+	
+	Vector operator+(Vector v);
+	Vector operator-(Vector v);
+	float operator*(Vector v);
+	Vector operator/(Vector v);
+
+private:
+
+	float x;
+	float y;
+	float z;
+
 };
 
+#endif // !Vector_H
