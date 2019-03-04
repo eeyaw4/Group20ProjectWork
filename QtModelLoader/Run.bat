08fc893faeb932100143cec4f57755debeec28de
TJ <eeyto2@nottingham.ac.uk>
2019-03-04 15:21:37 +0000

    integrating modelloader

--- QtModelLoader/Run.bat
+++ QtModelLoader/Run.bat
@@ -1,5 +1,6 @@
-cd C:\Users\pc\Documents\Year 2\Computer Group Project\Group20ProjectWork\QtModelLoader\Build
+cd C:\Users\tyler\Group20ProjectWork\QtModelLoader\Build
 set PATH=%PATH%;C:\Qt\5.11.1\msvc2017_64\bin;C:\VTK\MSVC2017\bin
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
 msbuild ModelLoader.sln
 cd Debug
 ModelLoader.exe
