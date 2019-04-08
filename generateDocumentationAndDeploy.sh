3782dfcf0fd72061edd947c7b2d70dbb08000830
Arron Wells <eeyaw4@nottingham.ac.uk>
2019-04-08 11:36:52 +0100

    change script

--- generateDocumentationAndDeploy.sh
+++ generateDocumentationAndDeploy.sh
@@ -15,12 +15,12 @@ git config user.email "travis@travis-ci.com"
 
 rm -rf *
 echo "" > .nojekyll
-doxygen $DOXYFILE 2>&1 | tee doxygen.log
+doxygen $DOXYFILE | tee doxygen.log
 if [ -d "html" ] && [ -f "html/index.html" ]; then
 
 	git add --all
 	git commit -m "Deploy docs: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-	git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+	git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null
 	
 else
 	echo '' >&2
