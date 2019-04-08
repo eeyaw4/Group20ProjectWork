5fff1afe791c17be286974ff830f55eaf12f7631
Arron Wells <eeyaw4@nottingham.ac.uk>
2019-04-08 12:05:09 +0100

    Documentation

--- QtModelLoader/mainwindow.cpp
+++ QtModelLoader/mainwindow.cpp
@@ -1,14 +1,9 @@
-// mainwindow.cpp
-#include "mainwindow.h"
-#include "ui_mainwindow.h"
-
 /** @file
 * This file contains all functions used in the mainwindow.cpp
 */
 
-/** @Brief description
-* Details all functions that are required to make the application run
-*/
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
