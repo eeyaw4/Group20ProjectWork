b5089027257f6dfe41c6b7aa5c1b645380355d58
Arron Wells <eeyaw4@nottingham.ac.uk>
2019-04-09 09:11:09 +0100

    Testing Code

--- QtModelLoader/LibraryCode/tests.cpp
+++ QtModelLoader/LibraryCode/tests.cpp
@@ -2,43 +2,7 @@
 
 #include "Model.h"
 
-TEST_CASE( "Model Library Test", "[ModelClass]" ) {
-	SECTION ("Loading A Material From The Model File"){
-		Model testModel;
-		testModel.readFile("../ExampleModel1.MOD");
-		
-		testModel.getMaterialData(0);
-		
-		REQUIRE( testModel.materialID == 0 );
-		REQUIRE( testModel.materialDensity == 8960 );
-		REQUIRE( testModel.materialColour == "b87333" );
-		REQUIRE( testModel.materialName == "copper" );
-	}
-	
-	SECTION ("Loading A Vertex From The Model File"){
-		Model testModel;
-		testModel.readFile("../ExampleModel1.MOD");
-		
-		testModel.getVertexData(6);
-		
-		vector<float> testVector;
-		
-		testVector.push_back(1.);
-		testVector.push_back(1.);
-		testVector.push_back(1.);
-		
-		REQUIRE( testModel.vertexID == 6 );
-		REQUIRE( testModel.vertexXYZ == testVector );
-	}
-	
-	SECTION ("Loading A Cell From The Model File"){
-		Model testModel;
-		testModel.readFile("../ExampleModel1.MOD");
-		
-		testModel.getCellData(1);
-		
-		REQUIRE( testModel.cellID == 1 );
-		REQUIRE( testModel.cellShape == "t" );
-		REQUIRE( testModel.cellMaterial == 1 );
-	}
+int main()
+{
+	std::cout << "Test Worked" << endl;
 }
